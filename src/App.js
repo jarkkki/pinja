b1eb2e3b08ea655ef841476c7fe3b0a17756862b
jarno <jarno.hantula@hotmail.com>
2024-11-24 23:07:13 +0200

    alku

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ConsultantsList from './components/ConsultantsList';
+import ConsultantProfile from './components/ConsultantProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ConsultantsList />} />
+        <Route path="/consultant/:id" element={<ConsultantProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
