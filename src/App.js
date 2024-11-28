8ca32c9894b6a52492d5d5998539a1029d0b4a69
Sam Smith <jarno.hantula@hotmail.com>
2024-11-28 22:31:07 +0200

    reitityksiä

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ConsultantsList from './components/ConsultantsList';
 import ConsultantProfile from './components/ConsultantProfile';
+import AdminPanel from './components/AdminPanel';
 
 function App() {
   return (
@@ -9,10 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<ConsultantsList />} />
         <Route path="/consultant/:id" element={<ConsultantProfile />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
